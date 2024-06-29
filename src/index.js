08f3fec3e9b8d3ff6348cd47341deac476d9e700
Taylor Ding <143913149+Taylor-Ding@users.noreply.github.com>
2024-06-29 14:09:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import DOCS from './help.html'
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -150,6 +149,8 @@ async function fetchToken(wwwAuthenticate, scope, authorization) {
   return await fetch(url, { method: "GET", headers: headers });
 }
 
+import DOCS from './help.html'
+ 
 // return docs
 if (url.pathname === "/") {
   return new Response(DOCS, {
